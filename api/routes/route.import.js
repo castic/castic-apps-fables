0df975e4d60aff115055577306cdfbacfadbd946
Che Joseph <josephc@fiu.edu>
2015-05-15 12:56:51 -0400

    finished quest import

--- api/routes/route.import.js
+++ api/routes/route.import.js
@@ -1,16 +1,20 @@
 var fs = require('fs'),
-	Converter = require("csvtojson").core.Converter;
+	Converter = require("csvtojson").core.Converter,
+	Quest;
 
 exports.setRoute = function (db) {
+	Quest = db.model('Quest');
+
 	return {
-		quests: importQuest
+		quests: importQuest,
+
 	}
 };
 
 importQuest = function (req, res) {
 	var file = req.files.file|| {}
 
-	console.log(file);
+	// console.log(file);
 
 	// Only do import for text/csv file type
 	if (file.mimetype == 'text/csv') {
@@ -18,17 +22,32 @@ importQuest = function (req, res) {
 		//end_parsed will be emitted once parsing finished 
 		getJSON(file.file).on("end_parsed",function(jsonObj){
 		    
-		    var convertedObj = {
-		    	json: jsonObj,
-		    	fileName: file.filename,
-		    	uuid: file.uuid
-		    };
+			var json = transformToQuest(jsonObj, '|');
 
-		    return res.send(convertedObj); 
+			importJSON(Quest, json, function (err, result) {
+				if (err) {
+					console.log(err);
+					res.send(err);
+				}
+				res.send(result);
+			});
 		});	
 	} else {
-		res.send({});
+		res.send('not a csv file');
 	}
+};
+
+transformToQuest = function (ary, delimiter) {
+	ary.forEach(function (item) {
+		if (item.tags) {
+			item.tags = item.tags.split(delimiter);
+		}
+		if (item.children) {
+			item.children = item.children.split(delimiter).map(Number);
+		}
+	});
+
+	return ary;
 };
 
 getJSON = function (pathToCSV) {
@@ -42,4 +61,15 @@ getJSON = function (pathToCSV) {
 	fileStream.pipe(csvConverter);
 
 	return csvConverter;
+};
+
+importJSON = function (model, data, cb) {
+
+	if (data.length > 0) {
+		// console.log(model.find({}, cb));
+		model.find({}).remove().exec(function (err) {
+			if (err) cb(err);
+			model.collection.insert(data, cb);
+		})
+	}
 };
