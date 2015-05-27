6900f7a1489067129a4ad6c05ec28be3fa00a4bd
Che Joseph <josephc@fiu.edu>
2015-05-27 16:58:58 -0400

    front-end functionality added for chapters

--- api/models/story.js
+++ api/models/story.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose'),
 
 var storySchema = new Schema({
 		title: String,
-		priority: String,
+		priority: {type: String, default: '1'},
 		hero: {type: Number, ref: 'Person'},
 		champion: {type: Number, ref: 'Person'},
 		contacts: [ {type: Number, ref: 'Person'} ],
@@ -13,6 +13,7 @@ var storySchema = new Schema({
 			entryType: String,
 			title: String,
 			content: String,
+			status: {type: String, default: 'incomplete'},
 			createdBy: {type: Number, ref: 'Person'},
 			completedBy: {type: Number, ref: 'Person'},
 			timeCreated: { type: Date, default: Date.now() },
